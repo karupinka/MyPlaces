057e3ba24b9226dda3e89f45723cc51ad5515fc6
Репина Надежда Радиевна <n.repina@cft.ru>
2020-07-06 10:12:29 +0300

    Initial Commit

---
+++ MyPlaces/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MyPlaces
+//
+//  Created by Репина Надежда on 06.07.2020.
+//  Copyright © 2020 Репина Надежда. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
