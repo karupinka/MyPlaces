3f482598a2d5d39c20ebd8375ee367fdf7b0cc22
Репина Надежда Радиевна <n.repina@cft.ru>
2020-07-07 12:44:31 +0300

    Custom Table View Cell

---
+++ MyPlaces/CustomTableViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  CustomTableViewCell.swift
+//  MyPlaces
+//
+//  Created by Репина Надежда on 07.07.2020.
+//  Copyright © 2020 Репина Надежда. All rights reserved.
+//
+
+import UIKit
+
+class CustomTableViewCell: UITableViewCell {
+
+	@IBOutlet weak var imageOfPlace: UIImageView!
+	@IBOutlet weak var nameLabel: UILabel!
+	@IBOutlet weak var locationLabel: UILabel!
+	@IBOutlet weak var typeLabel: UILabel!
+}
